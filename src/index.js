f5e06d4818cdb340304d517b025258a30aea5c09
prakashharvani <prakashharvani1997@gmail.com>
2024-01-28 01:03:17 +0530

    database connection added.

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { Sequelize, DataTypes } = require('sequelize');
+
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(bodyParser.json());
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'database.sqlite', // SQLite database file
+  });
+
+  const GroceryItem = sequelize.define('GroceryItem', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    inventory: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  });
+  
+  // Sync the model with the database
+  sequelize.sync().then(() => {
+    console.log('Database and tables created!');
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
